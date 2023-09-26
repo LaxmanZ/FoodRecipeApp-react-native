36b6e7954c2f3888b6d18d640c680fdf8777de65
LaxmanZ <bhajantrilaxman2001@gmail.com>
2023-09-26 15:18:26 +0530

    created welcomeScreen

---
+++ src/navigation/AppNavigator.js
@@ -0,0 +1,21 @@
+import React from 'react';
+import { StyleSheet, Text, View } from 'react-native';
+import { NavigationContainer } from '@react-navigation/native';
+import { createNativeStackNavigator } from '@react-navigation/native-stack';
+import WelcomeScreen from '../screens/WelcomeScreen';
+
+const Stack = createNativeStackNavigator();
+
+const AppNavigator = () => {
+  return (
+    <NavigationContainer>
+      <Stack.Navigator screenOptions={{ headerShown: false }}>
+        <Stack.Screen name="Welcome" component={WelcomeScreen} />
+      </Stack.Navigator>
+    </NavigationContainer>
+  );
+};
+
+export default AppNavigator;
+
+const styles = StyleSheet.create({});
---
+++ src/screens/WelcomeScreen.js
@@ -0,0 +1,46 @@
+import React from 'react';
+import { TouchableOpacity } from 'react-native';
+import { Image } from 'react-native';
+import { StyleSheet, Text, View } from 'react-native';
+
+const WelcomeScreen = () => {
+  return (
+    <View style={{ flex: 1, alignItems: 'center' }}>
+      <Image
+        style={{ marginTop: 20 }}
+        source={require('../../assets/images/welcome1.png')}
+      />
+      <Text style={{ color: '#f96163', fontSize: 22, fontWeight: 'bold' }}>
+        40k+ Premium Recipes
+      </Text>
+      <Text
+        style={{
+          fontSize: 42,
+          fontWeight: 'bold',
+          color: '#3c444c',
+          marginTop: 44,
+          marginBottom: 45,
+        }}
+      >
+        Cook Like A Chef
+      </Text>
+      <TouchableOpacity
+        style={{
+          backgroundColor: '#f96163',
+          borderRadius: 18,
+          paddingVertical: 18,
+          width: '80%',
+          alignItems: 'center',
+        }}
+      >
+        <Text style={{ fontSize: 18, color: '#fff', fontWeight: '700' }}>
+          Let's Go
+        </Text>
+      </TouchableOpacity>
+    </View>
+  );
+};
+
+export default WelcomeScreen;
+
+const styles = StyleSheet.create({});
