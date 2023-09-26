d92842149c579e830fea26fbbc283abd8b7ab1f7
LaxmanZ <bhajantrilaxman2001@gmail.com>
2023-09-26 16:33:04 +0530

    added categoriesfilter

---
+++ src/components/CategoriesFilter.js
@@ -0,0 +1,46 @@
+import React from 'react';
+import { StyleSheet, Text, View, ScrollView } from 'react-native';
+import { categories, colors } from '../Constant';
+
+const CategoriesFilter = () => {
+  return (
+    <View>
+      <ScrollView horizontal showsHorizontalScrollIndicator={false}>
+        {categories.map((category, index) => {
+          return (
+            <View
+              style={{
+                backgroundColor:
+                  index === 0 ? colors.COLOR_PRIMARY : colors.COLOR_LIGHT,
+                marginRight: 30,
+                borderRadius: 8,
+                paddingHorizontal: 16,
+                paddingVertical: 12,
+                marginVertical: 16,
+
+                shadowColor: '#000',
+                shadowOffset: { width: 0, height: 4 },
+                shadowOpacity: 0.1,
+                shadowRadius: 7,
+              }}
+            >
+              <Text
+                key={index}
+                style={{
+                  color: index === 0 && colors.COLOR_LIGHT,
+                  fontSize: 18,
+                }}
+              >
+                {category.category}
+              </Text>
+            </View>
+          );
+        })}
+      </ScrollView>
+    </View>
+  );
+};
+
+export default CategoriesFilter;
+
+const styles = StyleSheet.create({});
--- src/screens/RecipeListScreen.js
+++ src/screens/RecipeListScreen.js
@@ -2,13 +2,23 @@ import React from 'react';
 import { StyleSheet, Text, View, SafeAreaView } from 'react-native';
 import Header from '../components/Header';
 import SearchFilter from '../components/SearchFilter';
+import CategoriesFilter from '../components/CategoriesFilter';
 
 const RecipeListScreen = () => {
   return (
     <SafeAreaView style={{ flex: 1, marginHorizontal: 16 }}>
       {/* Render Header  */}
       <Header headerText={'Hi, Zoro'} headerIcon={'bell-o'} />
+
+      {/* Search Filter  */}
       <SearchFilter icon="search" placeHolder={'Search Recipe Here'} />
+
+      {/* Category Filter  */}
+      <View style={{marginTop: 15}}>
+        <Text style={{fontSize: 22, fontWeight: 'bold'}}>Categories</Text>
+        {/* Categories List  */}
+        <CategoriesFilter />
+      </View>
     </SafeAreaView>
   );
 };
